6da2292b2bae2a5adc752f1ed55bccfa82a17491
Aa-sheesh <aashish4912345@gmail.com>
2025-04-30 22:48:33 +0530

    deploying frontend on vercel and refactoring .env for serverless architecture

--- server/config/postgres.js
+++ server/config/postgres.js
@@ -1,15 +1,16 @@
+// config/postgres.js
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 export default sequelize;
